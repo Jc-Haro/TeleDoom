7734a9ce67ab1b2a3f1638b2a4f7163f1b0ef1b6
hf <hyan.fierros@uniat.edu.mx>
2024-06-20 19:51:29 -0600

    V3 Settings/Credits Funtional

--- Assets/Scripts/Ui/UI_MainMenu/MainMenuController.cs
+++ Assets/Scripts/Ui/UI_MainMenu/MainMenuController.cs
@@ -5,6 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+    public GameObject mainMenu;
+    public GameObject settings;
+    public GameObject credits;
+    public Animator CameraObject;
+
+    private void Start()
+    {
+             
+    }
     public void StartGame(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
@@ -12,9 +21,41 @@ public class MainMenuController : MonoBehaviour
 
     public void ExitGame()
     {
-        Debug.Log("Exit Game...");
-        Application.Quit();
+        #if UNITY_EDITOR
+        	UnityEditor.EditorApplication.isPlaying = false;
+		#else
+			Application.Quit();
+		#endif
     }
 
-    
+    public void ObjectOnOff(GameObject obon1, GameObject obon2){
+        obon2.SetActive(true);
+        StartCoroutine(TimeAndDelay());
+        obon1.SetActive(false);
+    }
+
+    public void SettingsOn(){
+		CameraObject.SetFloat("Settings 0",1);
+        ObjectOnOff(mainMenu, settings);
+	}
+        
+    public void SettingsOff(){
+    	CameraObject.SetFloat("Settings 0",0);
+        ObjectOnOff(settings, mainMenu);
+	}
+
+    public void CreditsOn(){
+		CameraObject.SetFloat("Credits 0",1);
+        ObjectOnOff(mainMenu, credits);
+	}
+        
+    public void CreditsOff(){
+    	CameraObject.SetFloat("Credits 0",0);
+        ObjectOnOff(credits, mainMenu);
+	}
+
+    private IEnumerator TimeAndDelay()
+    {
+        yield return new WaitForSeconds(1f);
+    }
 }
