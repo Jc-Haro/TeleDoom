cd4dbd88d63b5651bfb0a2cec266cc2a1c509fd4
Juan HaRo <jcharomo@gmail.com>
2024-07-02 15:36:34 -0600

    Add bullet particles

--- Assets/Scripts/Gun/WeaponSwitcher.cs
+++ Assets/Scripts/Gun/WeaponSwitcher.cs
@@ -1,10 +1,16 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class WeaponSwitcher : MonoBehaviour
 {
-    public List<GameObject> weapons;
+    [Serializable]
+    public struct Weapon
+    {
+        public GameObject weapon;
+        public Transform bulletStart;
+    }
+    public List<Weapon> weapons;
     private int lastIndex;
 
     void Start()
@@ -12,30 +18,30 @@ public class WeaponSwitcher : MonoBehaviour
         // First deactivates all weapons, just in case
         foreach (var weapon in weapons)
         {
-            weapon.SetActive(false);
+            weapon.weapon.SetActive(false);
         }
 
         // Activates a random weapon
-        lastIndex = Random.Range(0, weapons.Count);
-        weapons[lastIndex].SetActive(true);
+        lastIndex = UnityEngine.Random.Range(0, weapons.Count);
+        weapons[lastIndex].weapon.SetActive(true);
     }
 
     // Function to get a new random weapon, without repeating the last one
     public void GetRandomWeapon()
     {
         // Current weapon gets deactivated
-        weapons[lastIndex].SetActive(false);
+        weapons[lastIndex].weapon.SetActive(false);
 
         int newIndex;
 
         // Creates a new index for the new weapon, if its the same as the current weapon it will create a new one until its different
         do
         {
-            newIndex = Random.Range(0, weapons.Count);
+            newIndex = UnityEngine.Random.Range(0, weapons.Count);
         } while (newIndex == lastIndex);
 
         // The weapon index copies the new index and activates that weapon
         lastIndex = newIndex;
-        weapons[lastIndex].SetActive(true);
+        weapons[lastIndex].weapon.SetActive(true);
     }
 }
