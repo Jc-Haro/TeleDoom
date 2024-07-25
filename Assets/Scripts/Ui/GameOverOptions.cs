2aa0411fd4a91562eb7a7e6e08e1bdf418cef9e0
hf <hyan.fierros@uniat.edu.mx>
2024-07-25 01:07:30 -0600

    Menu_Pause_Finish

--- Assets/Scripts/Ui/GameOverOptions.cs
+++ Assets/Scripts/Ui/GameOverOptions.cs
@@ -6,7 +6,6 @@ using UnityEngine.SceneManagement;
 public class GameOverOptions : MonoBehaviour
 {
 
-
     public void Quit()
     {
         Application.Quit();
--- Assets/Scripts/Ui/UI_MainMenu/MainMenuController.cs
+++ Assets/Scripts/Ui/UI_MainMenu/MainMenuController.cs
@@ -2,12 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
     public GameObject mainMenu;
     public GameObject settings;
     public GameObject credits;
+
+    public Button buttonSettings;
+
+    public Button buttonCredits;
+
+    public Button buttonMenu;
+
     public Animator CameraObject;
 
     public void StartGame(string sceneName)
@@ -24,30 +32,31 @@ public class MainMenuController : MonoBehaviour
 		#endif
     }
 
-    public void ObjectOnOff(GameObject obon1, GameObject obon2){
+    public void ObjectOnOff(GameObject obon1, GameObject obon2, Button button){
         obon2.SetActive(true);
         StartCoroutine(TimeAndDelay());
         obon1.SetActive(false);
+        button.Select();
     }
 
     public void SettingsOn(){
 		CameraObject.SetFloat("Settings 0",1);
-        ObjectOnOff(mainMenu, settings);
+        ObjectOnOff(mainMenu, settings, buttonSettings);
 	}
         
     public void SettingsOff(){
     	CameraObject.SetFloat("Settings 0",0);
-        ObjectOnOff(settings, mainMenu);
+        ObjectOnOff(settings, mainMenu, buttonMenu);
 	}
 
     public void CreditsOn(){
 		CameraObject.SetFloat("Credits 0",1);
-        ObjectOnOff(mainMenu, credits);
+        ObjectOnOff(mainMenu, credits, buttonCredits);
 	}
         
     public void CreditsOff(){
     	CameraObject.SetFloat("Credits 0",0);
-        ObjectOnOff(credits, mainMenu);
+        ObjectOnOff(credits, mainMenu, buttonMenu);
 	}
 
     private IEnumerator TimeAndDelay()
